566110af9d519ff741344af2387942c391f95eb2
Camila Iglesias <apple@MacBook-de-Camila.local>
2023-08-17 16:04:59 -0500

    implementacion testing

--- src/usuarios/usuarios.controller.spec.ts
+++ src/usuarios/usuarios.controller.spec.ts
@@ -1,79 +1,41 @@
 /* eslint-disable prettier/prettier */
-
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsuariosController } from './usuarios.controller';
+import { UsuariosController } from "./usuarios.controller";
 import { UsuariosService } from './usuarios.service';
-import { getModelToken } from '@nestjs/mongoose';
-import { Usuario } from './interfaces/usuario.interface';
-import { CreateUsuarioDto } from './dto/usuario.dto';
-import { HttpStatus, NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Usuario } from "./interfaces/usuario.interface";
+import { CreateUsuarioDto } from "./dto/usuario.dto";
 
 describe('UsuariosController', () => {
   let usuariosController: UsuariosController;
   let usuariosService: UsuariosService;
 
-  const usuarioData: Usuario = {
-    cedula: '123456789',
-    nombre:
-    apellido:
-    telefono:
-    email:
-    // Other properties here...
-  };
-
-  const createUsuarioDto: CreateUsuarioDto = {
-    cedula: '123456789',
-    // Other properties here...
-  };
-
-  const usuariosServiceMock = {
-    getUsuarios: jest.fn(),
-    getUsuario: jest.fn(),
-    createUsuario: jest.fn(),
-    deleteUsuario: jest.fn(),
-    updateUsuario: jest.fn(),
-  };
-
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsuariosController],
-      providers: [
-        {
-          provide: UsuariosService,
-          useValue: usuariosServiceMock,
-        },
-        {
-          provide: getModelToken('Usuario'),
-          useValue: {}, // Mock or actual model if needed
-        },
-      ],
-    }).compile();
+    usuariosService = {
+      getUsuario: jest.fn(), // Mock the getUsuario method
+    } as unknown as UsuariosService;
 
-    usuariosController = module.get<UsuariosController>(UsuariosController);
-    usuariosService = module.get<UsuariosService>(UsuariosService);
+    usuariosController = new UsuariosController(usuariosService);
   });
 
-  describe('getUsuario', () => {
-    it('should return a user by cedula', async () => {
-      usuariosServiceMock.getUsuario.mockResolvedValue(usuarioData);
+  describe('findAll', () => {
+    it('retorna un array de usuarios', async () => {
+      //this is an example of the json
+      const result = [[
+        {
+            "_id": "",
+            "cedula": "",
+            "nombre": "",
+            "apellido": "",
+            "telefono": "",
+            "email": "",
+            "__v": 0
+        },
 
-      const result = await usuariosController.getUsuario('123456789');
-      
-      expect(result).toEqual({
-        status: HttpStatus.OK,
-        json: usuarioData,
-      });
-    });
-
-    it('should throw NotFoundException if user is not found', async () => {
-      usuariosServiceMock.getUsuario.mockResolvedValue(null);
-
-      await expect(usuariosController.getUsuario('987654321')).rejects.toThrowError(
-        new NotFoundException('El usuario no se encuentra en nuestros registros'),
-      );
+    ]];
+      jest.spyOn(usuariosService, 'getUsuario').mockImplementation(() =>
+        Promise.resolve(result as unknown as Promise<Usuario>));
+        expect(await usuariosController.getUsuario(200, '1027892323'));
+    })
     });
   });
-
-  // Add other test cases for the remaining controller methods (createUsuario, deleteUsuario, updateUsuario) here
-});
-
